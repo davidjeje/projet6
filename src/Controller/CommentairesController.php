52fdfbc4120f22e73cb814eb19fc6a73b2d79993
Davidjeje <david.marivat-larose@orange.fr>
2019-02-28 15:50:22 +0100

    Update CommentairesController.php file

--- src/Controller/CommentairesController.php
+++ src/Controller/CommentairesController.php
@@ -11,6 +11,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\FileUploader;
 use Symfony\Component\HttpFoundation\File\Exception\FileException; 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UsersRepository;
 
 /**
  * @Route("/commentaires")
