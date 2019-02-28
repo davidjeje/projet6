2769749072c801d441c96a787bbd3cd974dc34e5
Davidjeje <david.marivat-larose@orange.fr>
2019-02-28 15:52:47 +0100

    Update UsersController.php file

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UsersType;
+use App\Form\UserType;
+use App\Form\UserShowType;
 use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,9 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\Exception\FileException; 
 use App\Entity\Tricks;
 use App\Form\TricksType;
 use App\Repository\TricksRepository;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 
 
@@ -34,45 +38,38 @@ class UsersController extends AbstractController
     /**
      * @Route("/new", name="users_new", methods="GET|POST")
      */
-    public function new(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function new(Request $request, UserPasswordEncoderInterface $passwordEncoder, \Swift_Mailer $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(UsersType::class, $user);
         $form->handleRequest($request);
         $email = $user->getEmail();
+        $token = $user->getToken();
 
         if ($form->isSubmitted() && $form->isValid()) {
 
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
             //on active par défaut
-            $user->setIsActive(true);
+            $user->setIsActive(false);
             //$user->addRole("ROLE_ADMIN");
             // 4) save the User!
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
-            $message = (new \Swift_Message('Nous vous souhaitons la bienvenu. Votre compte utilisateur est validé !'))
+            $message = (new \Swift_Message('Nous vous souhaitons la bienvenu. Clic sur le lien pour valider ton inscription ! A bientôt !!!'))
             ->setFrom('dada.pepe.alal@gmail.com')
             ->setTo($email)
             ->setBody(
             $this->renderView(
                 // templates/emails/registration.html.twig
                 'users/texte.html.twig',
-                ['name' => $name]
+                ['token' => $token]
+
             ),
             'text/html'
         )
-        /*
-         * If you also want to include a plaintext version of the message
-        ->addPart(
-            $this->renderView(
-                'emails/registration.txt.twig',
-                ['name' => $name]
-            ),
-            'text/plain'
-        )
-        */
+        
     ;
 
     $mailer->send($message);
@@ -87,6 +84,51 @@ class UsersController extends AbstractController
             'user' => $user,
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/validate/{token}", name="validate", methods="GET|POST")
+     */
+    public function validateAccount($token, UsersRepository $usersRepository)
+    {
+        $user = $usersRepository->findOneBy(array("token"=>$token));
+
+        $user->setIsActive(true);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        $session = new Session();
+        $session->start();
+        return $this->redirectToRoute('login');
+    }
+    /**
+     * @Route("/change/{token}", name="changePassword", methods="GET|POST")
+     */
+    public function changePassword($token, UsersRepository $usersRepository, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = $usersRepository->findOneBy(array("token"=>$token));
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            //on active par défaut
+            $user->setIsActive(true);
+            //$user->addRole("ROLE_ADMIN");
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+             return $this->redirectToRoute('login');
+            
+        }
+
+        return $this->render('users/changePass.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+            ]); 
     }
 
     /**
@@ -105,17 +147,77 @@ class UsersController extends AbstractController
                 ->add('_password', \Symfony\Component\Form\Extension\Core\Type\PasswordType::class, ['label' => 'Mot de passe'])
                 ->add('ok', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Ok', 'attr' => ['class' => 'btn-primary btn-block']])
                 ->getForm();
-        return $this->render('connexion.html.twig', [
-                    'mainNavLogin' => true, 'title' => 'Connexion',
-                    //
+                
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $session = new Session();
+            $session->start();
+            return $this->redirectToRoute('tricks_index');
+        }
+        return $this->render('users/connexion.html.twig', [
+                    'mainNavLogin' => true, 
+                    'title' => 'Connexion',
                     'form' => $form->createView(),
                     'last_username' => $lastUsername,
                     'error' => $error,
         ]);
-        /*if ($form->isSubmitted() && $form->isValid()) 
-        {
+        
+    }
+
+
+    /**
+     * @Route("/forgot", name="forgot", methods="GET|POST")
+     */
+    public function forgot(Request $request, \Swift_Mailer $mailer, UsersRepository $usersRepository) {
+        
+            $form = $this->get('form.factory')
+            ->createNamedBuilder(null)
+            ->add('_username', null, ['label' => 'Email'])
+            ->add('ok', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Réinitialiser le mot de passe', 'attr' => ['class' => 'btn-primary btn-block']])
+            ->getForm();
+            $form->handleRequest($request);
+          if ($form->isSubmitted() && $form->isValid()) {
+
+            $email = $request->request->get('_username');
+            $user = $usersRepository->findOneBy(array("email"=>$email));
+            $user->setToken(bin2hex(random_bytes(16)));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            $message = (new \Swift_Message('Nous vous souhaitons la bienvenu. Cliquer sur le lien pour pouvoir Réinitialiser votre mot de passe ! A bientôt !!!'))
+            ->setFrom('dada.pepe.alal@gmail.com')
+            ->setTo($email)
+            ->setBody(
+            $this->renderView(
+                
+                'users/reset.html.twig',
+                ['token' => $user->getToken()]
+
+            ),
+            'text/html'
+        )
+        
+    ;
+
+    $mailer->send($message);
+            
+            $this->addFlash('o', 'Votre compte à bien été enregistré.');
+
             return $this->redirectToRoute('tricks_index');
-        }*/
+        }
+        
+
+        
+        
+        return $this->render('users/forgot.html.twig', [
+                    'mainNavLogin' => true, 'title' => 'Mot de passe oublier',
+                   
+                    'form' => $form->createView(),
+                    
+                    'error' => null,
+        ]);
+
+        
     }
 
     /**
@@ -143,6 +245,21 @@ class UsersController extends AbstractController
         return $this->render('users/edit.html.twig', [
             'user' => $user,
             'form' => $form->createView(),
+        ]);
+    }
+
+    
+
+    /**
+     * @Route("/logout", name="logout", methods="GET|POST")
+     */
+    public function logout(Request $request, User $user): Response
+    {
+        $session->destroy();
+        return $this->render('tricks/index.html.twig', [
+                    'mainNavLogin' => false, 
+                    'title' => 'Deconnexion',
+                    'error' => null,
         ]);
     }
 
