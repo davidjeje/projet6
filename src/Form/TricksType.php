a2b8458b8a6e50de15082eaf3c427f679947c640
Davidjeje <david.marivat-larose@orange.fr>
2019-02-28 15:56:53 +0100

    Update the form tricks

--- src/Form/TricksType.php
+++ src/Form/TricksType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
 
 
 class TricksType extends AbstractType
@@ -18,15 +20,34 @@ class TricksType extends AbstractType
             ->add('description')
             ->add('level')
             ->add('groupname')
-            ->add('image', FileType::class, array('label' => 'Tricks (png image)','data_class' => null))
+            ->add('image', FileType::class, array('label' => 'Tricks (jpeg, jpg, png image)','data_class' => null))
+            ->add('video', UrlType::class, array('label' => 'Video (AVI, MP4, MKV…)'))
+
         
         ;
     }
+
+   /* public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('level')
+            ->add('groupname')
+            ->add('image', FileType::class, array('label' => 'Tricks (jpeg, jpg, png image)','data_class' => null))
+            ->add('video', CollectionType::class, [
+                'entry_type' =>UrlType::class,
+                'entry_options' => ['label' => true],
+                'allow_add' => true,
+            ]);
+    }*/
+
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Tricks::class,
+            //'empty_data ' => getImage(),
         ]);
     }
 
