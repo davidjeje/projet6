98d7937c8eba93441ceafaa8ef8bb0bb90565e86
Davidjeje <david.marivat-larose@orange.fr>
2019-02-28 16:01:59 +0100

    Update UsersType.php file

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -11,13 +11,17 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class UsersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+                ->add('prenom')
+                ->add('nom')
                 ->add('email', EmailType::class)
+                ->add('photo', FileType::class, array('label' => 'Users (jpeg, jpg, png image)','data_class' => null))
                 ->add('plainPassword', RepeatedType::class, array(
                     'type' => PasswordType::class,
                     'first_options' => array('label' => 'Mot de passe'),
