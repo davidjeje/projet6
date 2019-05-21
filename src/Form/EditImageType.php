48b413771c68b460b32c7766e72b16f3de50f50e
Davidjeje <david.marivat-larose@orange.fr>
2019-05-21 02:31:37 +0200

    adding file EditImageType.php

---
+++ src/Form/EditImageType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tricks;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+
+class EditImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            
+        ->add($options['image'], FileType::class, ['attr' => ['class' => 'form-control']]);
+        
+    }
+
+
+
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Tricks::class,
+            'image' => null,
+        ]);
+    }
+
+    
+}
