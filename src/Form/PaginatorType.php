dcf21f921eef9847638315d1f526f60877258c53
Davidjeje <david.marivat-larose@orange.fr>
2019-05-21 02:33:03 +0200

    adding file PaginatorType.php

---
+++ src/Form/PaginatorType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Paginator;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PaginatorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('page')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Paginator::class,
+        ]);
+    }
+}
